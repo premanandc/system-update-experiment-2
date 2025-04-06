16f3e72143171e4093e5e19cfa1080b39a31e0fd
System Admin <user@example.com>
2025-04-06 15:06:57 -0400

    Update Stryker configuration to use wildcards for model files

--- stryker.conf.js
+++ stryker.conf.js
@@ -7,12 +7,7 @@ module.exports = {
   testRunner: 'jest',
   coverageAnalysis: 'perTest',
   mutate: [
-    'src/models/package.ts',
-    'src/models/update.ts',
-    'src/models/device.ts',
-    'src/models/batch.ts',
-    'src/models/plan.ts',
-    'src/models/execution.ts'
+    'src/models/**/*.ts'
   ],
   timeoutMS: 60000,
   concurrency: 4,
