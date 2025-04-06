06e144bcb44a64992593a8700ad418f6483be7f1
System Admin <user@example.com>
2025-04-06 14:16:42 -0400

    Add Stryker mutation testing as a fitness function

---
+++ stryker.conf.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  packageManager: 'npm',
+  reporters: ['html', 'clear-text', 'progress'],
+  testRunner: 'jest',
+  coverageAnalysis: 'perTest',
+  mutate: ['src/models/package.ts'],
+  timeoutMS: 60000,
+  concurrency: 4,
+  ignorePatterns: ['node_modules', 'dist', 'coverage'],
+}; 
